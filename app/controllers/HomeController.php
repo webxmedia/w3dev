59c3391a90f70f46341baddbb99e15ba64bdb092
webxmedia <WEBxMedia@Matts-iMac-2.local>
2014-08-21 00:39:05 +0100

    Created basic index view

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,9 +15,10 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
-	{
-		return View::make('hello');
-	}
+    public function index() {
+
+        return View::make('index');
+
+    }
 
 }
